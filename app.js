47333b2bf1a8db1d1831ae211eedaf7654983ded
Ethan Hansen <hanseneg@hotmail.com>
2020-09-08 15:10:04 -0600

    commit4

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+    // adds all the numbers together in a string with reduce
 // const nums = [1, 2, 3, 4]
 // const result = nums.reduce(function(final, num){
 //     final += num
@@ -5,6 +6,7 @@
 // })
 // console.log(result)
 
+    // creates a string of one large number from an array of numbers using reduce
 // const result = nums.reduce(function(final, num){
 //     const string = num.toString()
 //     final += string
@@ -27,7 +29,7 @@
 //     {name: 'Zack', age: 19, voted: false}
 // ]
 
-//count items with reduce
+    //count items with reduce
 // const count = voters.reduce(function(final, voter){
 //     if(voter.voted){                    //simply insert ! in front of voter on this line to count how many didn't vote 
 //         final++
@@ -44,7 +46,7 @@
 //     { title: "A second Tesla Model S", price: 90000 }
 // ]
 
-//add items with reduce
+    //add items with reduce
 // const result = wishlist.reduce(function(final, item){
 //     final += item.price
 //     return final
@@ -57,9 +59,44 @@
 //     [4, 5, 6]
 // ]
 
-// creates one array from multiple arrays
+    // creates one array from multiple arrays with reduce and concat
 // const result = arrays.reduce(function(final, array){
 //     const newArr = final.concat(array)
 //     return newArr
 // }, [])
 // console.log(result)
+
+var voters = [
+    {name:'Bob' , age: 30, voted: true},
+    {name:'Jake' , age: 32, voted: true},
+    {name:'Kate' , age: 25, voted: false},
+    {name:'Sam' , age: 20, voted: false},
+    {name:'Phil' , age: 21, voted: true},
+    {name:'Ed' , age:55, voted:true},
+    {name:'Tami' , age: 54, voted:true},
+    {name: 'Mary', age: 31, voted: false},
+    {name: 'Becky', age: 43, voted: false},
+    {name: 'Joey', age: 41, voted: true},
+    {name: 'Jeff', age: 30, voted: true},
+    {name: 'Zack', age: 19, voted: false}
+]
+const result = voters.reduce(function(final, voter){
+    if(voter.age < 26){
+        final.youngPeople++
+        if(voter.voted){
+            final.youngVotes++
+        }
+    } else if(voter.age < 36){
+        final.midPeople++
+        if(voter.voted){
+            final.midVotes++
+        }
+    } else if(voter.age < 56){
+        final.oldPeople++
+        if(voter.voted){
+            final.oldVotes++
+        }
+    }
+    return final
+}, {youngVotes: 0, youngPeople: 0, midVotes: 0, midPeople: 0, oldVotes: 0, oldPeople: 0})
+console.log(result)
